67bae15a294dceafc9697f44f38f2cdebc3bbd79
DBaw <dbawolek95@gmail.com>
2024-10-15 17:21:57 +0200

    Add Material.Avalonia

--- ViewModels/SingleNotePageViewModel.cs
+++ ViewModels/SingleNotePageViewModel.cs
@@ -1,6 +1,32 @@
-﻿namespace ToDoApp.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ToDoApp.Dto;
+
+namespace ToDoApp.ViewModels
 {
     public partial class SingleNotePageViewModel : ViewModelBase
     {
+        [ObservableProperty]
+        public NoteDto _selectedNote;
+
+        [ObservableProperty]
+        public string _noteTitle;
+
+        [ObservableProperty]
+        public string _noteContent;
+
+        [ObservableProperty]
+        public bool _isReadOnlyMode;
+
+        public SingleNotePageViewModel()
+        {
+            if (SelectedNote == null) 
+            {
+                NoteTitle = "Select Note"; 
+            }
+
+            IsReadOnlyMode = true;
+
+        }
+
     }
 }
